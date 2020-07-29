e394d4214908e291bfaf24a9e393dde10ea58fe8
svandenb080618 <simon.vandenbulcke@socgen.com>
2020-07-29 22:55:38 +0200

    [feat] Create UI with all label and actions needed

--- timerCopro/ViewController.swift
+++ timerCopro/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var minutesLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        roundButton()
     }
 
-
+    @IBAction func startTimer(_ sender: Any) {
+    }
+    @IBAction func pauseTimer(_ sender: Any) {
+    }
+    @IBAction func resetTimer(_ sender: Any) {
+    }
+    private func roundButton() {
+        resetButton.layer.cornerRadius = resetButton.layer.frame.height/2
+        startButton.layer.cornerRadius = startButton.layer.frame.height/2
+        pauseButton.layer.cornerRadius = pauseButton.layer.frame.height/2
+        
+    
+    }
 }
 
