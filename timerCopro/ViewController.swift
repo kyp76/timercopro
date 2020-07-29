bc39605310cc54e348a04f0e195636b0999953c2
svandenb080618 <simon.vandenbulcke@socgen.com>
2020-07-29 22:21:04 +0200

    Initial Commit

---
+++ timerCopro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timerCopro
+//
+//  Created by simon vandenbulcke on 29/07/2020.
+//  Copyright © 2020 simon vandenbulcke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
