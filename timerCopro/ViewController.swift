891499e3392baa19a7543ed4f7026894a206450a
svandenb080618 <simon.vandenbulcke@socgen.com>
2020-08-02 15:05:09 +0200

    Code implementation to start pause reset timer, and link this code with UI

--- timerCopro/ViewController.swift
+++ timerCopro/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var time: Int = 0
+    var timer = Timer()
+    
     @IBOutlet weak var hourLabel: UILabel!
     @IBOutlet weak var minutesLabel: UILabel!
     @IBOutlet weak var secondLabel: UILabel!
@@ -23,19 +26,41 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         roundButton()
     }
-
+ 
     @IBAction func startTimer(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerDidEnded) , userInfo: nil, repeats: true)
     }
     @IBAction func pauseTimer(_ sender: Any) {
+        timer.invalidate()
     }
     @IBAction func resetTimer(_ sender: Any) {
+        timer.invalidate()
+        time = 0
+        
+        updateUI()
     }
     private func roundButton() {
         resetButton.layer.cornerRadius = resetButton.layer.frame.height/2
         startButton.layer.cornerRadius = startButton.layer.frame.height/2
         pauseButton.layer.cornerRadius = pauseButton.layer.frame.height/2
-        
+    }
     
+    @objc private func timerDidEnded() {
+        time += 1
+        updateUI()
+    }
+    private func updateUI() {
+        var hour : Int
+        var min : Int
+        var sec : Int
+        
+        hour = time/(60*60)
+        min = (time/60)%60
+        sec = time%60
+        
+        hourLabel.text = String(hour)
+        minutesLabel.text = String(min)
+        secondLabel.text = String(sec)
     }
 }
 
